cd43fda6919dfc8e370135086ddd3640113c00c0
marcope-98 <marco.eugenio.peressutti@gmail.com>
2026-04-24 14:26:48 +0200

    [inca/unique_com_ptr] Adds std::unique_ptr alias with custom deleter for COM objects

---
+++ src/inca/include/inca/unique_com_ptr.hpp
@@ -0,0 +1,23 @@
+#ifndef INCA_UNIQUE_COM_PTR_HPP_
+#define INCA_UNIQUE_COM_PTR_HPP_
+
+#include <memory>
+
+namespace inca
+{
+  template<class T>
+  struct ComObjDeleter
+  {
+    void operator()(T *ptr)
+    {
+      if (ptr == nullptr) return;
+      ptr->Release();
+    }
+  };
+
+  template<class T>
+  using unique_com_ptr = std::unique_ptr<T, ComObjDeleter<T>>;
+
+}; // namespace inca
+
+#endif // INCA_UNIQUE_COM_PTR_HPP_
