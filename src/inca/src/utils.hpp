958de18465279cb6ae0ad3e44861c9a7b57fc5ee
marcope-98 <marco.eugenio.peressutti@gmail.com>
2026-04-24 14:51:42 +0200

    [inca/utils] Adds QueryInterface wrapper utility to directly return inca::unique_com_ptr

---
+++ src/inca/src/utils.hpp
@@ -0,0 +1,28 @@
+#ifndef INCA_UTILS_HPP_
+#define INCA_UTILS_HPP_
+
+#include <stdexcept>
+
+#include <comdef.h>
+
+#include "inca/unique_com_ptr.hpp"
+
+namespace inca
+{
+  template<class T>
+  auto query_interface(IDispatch *src) -> unique_com_ptr<T>
+  {
+    if (src == nullptr)
+      throw std::invalid_argument("Attempt on calling QueryInterface on nullptr");
+
+    T      *out{nullptr};
+    HRESULT hr = src->QueryInterface(__uuidof(T), reinterpret_cast<void **>(&out));
+
+    if (FAILED(hr))
+      throw std::runtime_error("QueryInterface failed.");
+
+    return unique_com_ptr<T>(out);
+  }
+} // namespace inca
+
+#endif // INCA_UTILS_HPP_
