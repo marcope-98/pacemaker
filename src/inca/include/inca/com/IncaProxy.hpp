1b28efbfea19ee80209b192adba6b6ef3c4e0df4
marcope-98 <marco.eugenio.peressutti@gmail.com>
2026-04-26 18:52:23 +0200

    [inca/com] Applies COMProxy trait class to Inca ActiveX instantiation proxy class

--- src/inca/include/inca/com/IncaProxy.hpp
+++ src/inca/include/inca/com/IncaProxy.hpp
@@ -2,27 +2,21 @@
 #define INCA_COM_INCAPROXY_HPP_
 
 #include "inca/unique_com_ptr.hpp"
+#include "inca/traits/COMProxy.hpp"
 
 struct Inca_Dispatch;
 struct IDispatch;
 
 namespace inca::com
 {
-  class IncaProxy
+  class IncaProxy : private inca::traits::COMProxy<::Inca_Dispatch>
   {
   public:
     IncaProxy();
     ~IncaProxy();
-    IncaProxy(const IncaProxy &)                = delete;
-    IncaProxy(IncaProxy &&) noexcept            = delete;
-    IncaProxy &operator=(const IncaProxy &)     = delete;
-    IncaProxy &operator=(IncaProxy &&) noexcept = delete;
 
     auto GetOpenedExperiment() -> ::IDispatch *;
     auto GetOpenedExperimentView() -> ::IDispatch *;
-
-  private:
-    inca::unique_com_ptr<::Inca_Dispatch> p_subject{nullptr};
   };
 } // namespace inca::com
 #endif // INCA_COM_INCAPROXY_HPP_
