0342ad061dbb49c0ba7ed42657afa65ed8875212
marcope-98 <marco.eugenio.peressutti@gmail.com>
2026-04-28 12:05:26 +0200

    [inca/Experiment] Adds Experiment class to handle INCA Experiment interactions

---
+++ src/inca/include/inca/Experiment.hpp
@@ -0,0 +1,39 @@
+#ifndef INCA_EXPERIMENT_HPP_
+#define INCA_EXPERIMENT_HPP_
+
+#include <chrono>
+#include <string>
+
+#include "inca/com/ExperimentDeviceProxy.hpp"
+#include "inca/com/IncaExperimentViewProxy.hpp"
+#include "inca/com/IncaOnlineExperimentProxy.hpp"
+
+namespace inca
+{
+  class Experiment
+  {
+  public:
+    Experiment(inca::com::IncaOnlineExperimentProxy exp,
+               inca::com::IncaExperimentViewProxy   expview,
+               inca::com::ExperimentDeviceProxy     device);
+
+    Experiment(const Experiment &)            = delete;
+    Experiment &operator=(const Experiment &) = delete;
+    Experiment(Experiment &&)                 = default;
+    Experiment &operator=(Experiment &&)      = default;
+
+    ~Experiment() = default;
+
+    auto start_recording() -> void;
+    auto stop_recording(const std::string &filename) -> void;
+
+  private:
+    inca::com::IncaOnlineExperimentProxy m_exp;
+    inca::com::IncaExperimentViewProxy   m_expview;
+    inca::com::ExperimentDeviceProxy     m_device;
+
+    static constexpr std::chrono::milliseconds k_flush_delay{40};
+  };
+} // namespace inca
+
+#endif // INCA_EXPERIMENT_HPP_
---
+++ src/inca/src/Experiment.cpp
@@ -0,0 +1,25 @@
+#include "inca/Experiment.hpp"
+#include <thread>
+
+inca::Experiment::Experiment(
+    inca::com::IncaOnlineExperimentProxy exp,
+    inca::com::IncaExperimentViewProxy   expview,
+    inca::com::ExperimentDeviceProxy     device)
+    : m_exp{std::move(exp)},
+      m_expview{std::move(expview)},
+      m_device{std::move(device)}
+{
+}
+
+auto inca::Experiment::start_recording() -> void
+{
+  this->m_exp.StartRecording();
+}
+
+auto inca::Experiment::stop_recording(const std::string &filename) -> void
+{
+  (void)(filename);
+  std::this_thread::sleep_for(this->k_flush_delay);
+  this->m_exp.StopRecordingAndSave();
+  this->m_exp.StopMeasurement();
+}
