a7f019b21c80782535633c7ed276b0821790fab5
marcope-98 <marco.eugenio.peressutti@gmail.com>
2026-04-29 16:17:35 +0200

    [requirements] Modifies make_requirements script to generate documents in renamed build folder

--- requirements/make_requirements.bat
+++ requirements/make_requirements.bat
@@ -2,7 +2,7 @@
 setlocal
 set "cwd=%cd%"
 cd /d %~dp0
-mkdir build\chapters build\lib
-latexmk -synctex=1 -file-line-error -pdf -interaction=nonstopmode -outdir="%~dp0\build" "%~dp0\pacemaker_SRS.tex"
-@REM latexmk -synctex=1 -file-line-error -pdf -interaction=nonstopmode -outdir="%~dp0\build" "%~dp0\pacemaker_tests.tex"
+mkdir latexmk-\chapters\REQ latexmk-\chapters\TC latexmk-\lib
+latexmk -synctex=1 -file-line-error -pdf -interaction=nonstopmode -outdir="%~dp0\latexmk-build" "%~dp0\pacemaker_SRS.tex"
+latexmk -synctex=1 -file-line-error -pdf -interaction=nonstopmode -outdir="%~dp0\latexmk-build" "%~dp0\pacemaker_TS.tex"
 cd /d "%cwd%"
