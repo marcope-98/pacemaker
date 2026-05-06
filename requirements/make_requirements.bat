8b83a1282705549db6bee2eff96dd89d86d827e0
marcope-98 <marco.eugenio.peressutti@gmail.com>
2026-05-06 12:07:16 +0200

    [requirements] Implements custom cmake command and target to build SRS and TC pdf document in favor of bat file

--- requirements/make_requirements.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set "cwd=%cd%"
-cd /d %~dp0
-mkdir latexmk-\chapters\REQ latexmk-\chapters\TC latexmk-\lib
-latexmk -synctex=1 -file-line-error -pdf -interaction=nonstopmode -outdir="%~dp0\latexmk-build" "%~dp0\pacemaker_SRS.tex"
-latexmk -synctex=1 -file-line-error -pdf -interaction=nonstopmode -outdir="%~dp0\latexmk-build" "%~dp0\pacemaker_TS.tex"
-cd /d "%cwd%"
