8463c1219928a40ce3e5b1658d65c0e7082c5438
marcope-98 <marco.eugenio.peressutti@gmail.com>
2026-05-11 10:15:59 +0200

    [tests/TC-002] Finalizes test cases for REQ-002: pacemaker/inca/detail/COMProxy

---
+++ tests/pacemaker/inca/TC-002.cpp
@@ -0,0 +1,46 @@
+#include <type_traits>
+
+#include <gtest/gtest.h>
+
+#include "pacemaker/inca/detail/COMProxy.hpp"
+#include "pacemaker/inca/detail/unique_com_ptr.hpp"
+
+namespace
+{
+  std::size_t counter{};
+  struct MockCOM
+  {
+    void Release() { counter++; }
+  };
+
+  class ConcreteProxy : private pacemaker::inca::detail::COMProxy<MockCOM>
+  {
+  public:
+    ConcreteProxy(pacemaker::inca::detail::unique_com_ptr<MockCOM> ptr)
+        : COMProxy{std::move(ptr)} {}
+    ConcreteProxy(ConcreteProxy &&)            = default;
+    ConcreteProxy &operator=(ConcreteProxy &&) = default;
+    ~ConcreteProxy() {}
+  };
+
+} // namespace
+
+TEST(TC002, A)
+{
+  EXPECT_FALSE(std::is_copy_constructible_v<ConcreteProxy>);
+  EXPECT_TRUE(std::is_move_constructible_v<ConcreteProxy>);
+  EXPECT_FALSE(std::is_copy_assignable_v<ConcreteProxy>);
+  EXPECT_TRUE(std::is_move_assignable_v<ConcreteProxy>);
+}
+
+TEST(TC002, B)
+{
+  counter = 0;
+  {
+    pacemaker::inca::detail::unique_com_ptr<MockCOM> mock_unique_ptr{new MockCOM};
+    EXPECT_EQ(counter, 0);
+    auto concrete_proxy = ConcreteProxy{std::move(mock_unique_ptr)};
+    EXPECT_EQ(counter, 0);
+  }
+  EXPECT_EQ(counter, 1);
+}
