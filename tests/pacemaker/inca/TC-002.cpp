6431656d567078a376a87c1338325a9892b9dbe7
marcope-98 <marco.eugenio.peressutti@gmail.com>
2026-05-26 17:26:05 +0200

    [tests] Updates and expand test cases for COMProxy class  according to test case specification document

--- tests/pacemaker/inca/TC-002.cpp
+++ tests/pacemaker/inca/TC-002.cpp
@@ -1,32 +1,44 @@
 #include <type_traits>
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
+
+#include <comdef.h>
 
 #include "pacemaker/fixture/LeakTestFixture.hpp"
 
 #include "pacemaker/inca/detail/COMProxy.hpp"
 #include "pacemaker/inca/detail/unique_com_ptr.hpp"
 
+using ::testing::_;
+using ::testing::Return;
+
 PACEMAKER_FIXTURE_INIT(TC002)
 
 namespace
 {
-  std::size_t counter{};
-  struct MockCOM
+  struct MockCOM : public IDispatch
   {
-    void Release() { counter++; }
+    using type = IDispatch;
+
+    MOCK_METHOD(ULONG, AddRef, (), (override));
+    MOCK_METHOD(ULONG, Release, (), (override));
+    MOCK_METHOD(HRESULT, GetTypeInfo, (UINT, LCID, ITypeInfo **), (override));
+    MOCK_METHOD(HRESULT, GetTypeInfoCount, (UINT *), (override));
+    MOCK_METHOD(HRESULT, GetIDsOfNames, (const IID &, LPOLESTR *, UINT, LCID, DISPID *), (override));
+    MOCK_METHOD(HRESULT, Invoke, (DISPID, const IID &, LCID, WORD, DISPPARAMS *, VARIANT *, EXCEPINFO *, UINT *), (override));
+    MOCK_METHOD(HRESULT, QueryInterface, (const IID &, void **), (override));
+
+    MOCK_METHOD(void, func, ());
+
+    template<class F>
+    void Delegate_QueryInterface(const F &QueryInterface)
+    {
+      ON_CALL(*this, QueryInterface(_, _)).WillByDefault(QueryInterface);
+    }
   };
 
-  class ConcreteProxy : private pacemaker::inca::detail::COMProxy<MockCOM>
-  {
-  public:
-    ConcreteProxy(pacemaker::inca::detail::unique_com_ptr<MockCOM> ptr)
-        : COMProxy{std::move(ptr)} {}
-    ConcreteProxy(ConcreteProxy &&)            = default;
-    ConcreteProxy &operator=(ConcreteProxy &&) = default;
-    ~ConcreteProxy() {}
-  };
-
+  using ConcreteProxy = pacemaker::inca::detail::COMProxy<MockCOM>;
 } // namespace
 
 TEST_F(TC002, A)
@@ -39,13 +51,79 @@ TEST_F(TC002, A)
 
 TEST_F(TC002, B)
 {
-  counter = 0;
-  {
-    MockCOM                                          mock;
-    pacemaker::inca::detail::unique_com_ptr<MockCOM> mock_unique_ptr{&mock};
-    EXPECT_EQ(counter, 0);
-    auto concrete_proxy = ConcreteProxy{std::move(mock_unique_ptr)};
-    EXPECT_EQ(counter, 0);
-  }
-  EXPECT_EQ(counter, 1);
+  MockCOM mock;
+  mock.Delegate_QueryInterface([&mock](const IID &, void **out)
+                               {*out = reinterpret_cast<void*>(&mock); return S_OK; });
+  EXPECT_CALL(mock, AddRef()).Times(1);
+  EXPECT_CALL(mock, Release()).Times(2);
+  EXPECT_CALL(mock, QueryInterface(_, _)).Times(1);
+
+  EXPECT_CALL(mock, func()).Times(1);
+
+  mock.AddRef();
+  ConcreteProxy proxy{&mock};
+  EXPECT_NO_THROW(proxy->func());
+}
+
+TEST_F(TC002, C)
+{
+  ConcreteProxy proxy{};
+  EXPECT_EQ(proxy.get(), nullptr);
+}
+
+TEST_F(TC002, D)
+{
+  MockCOM mock;
+  mock.Delegate_QueryInterface([&mock](const IID &, void **out)
+                               {*out = reinterpret_cast<void*>(&mock); return S_OK; });
+  EXPECT_CALL(mock, QueryInterface(_, _)).Times(1);
+  EXPECT_CALL(mock, AddRef()).Times(1);
+  EXPECT_CALL(mock, Release()).Times(2);
+
+  mock.AddRef();
+  IDispatch *idispatch = &mock;
+  EXPECT_NO_THROW(ConcreteProxy proxy{idispatch});
+}
+
+TEST_F(TC002, E)
+{
+  MockCOM mock;
+  mock.Delegate_QueryInterface([&mock](const IID &, void **out)
+                               {*out = reinterpret_cast<void*>(&mock); return S_OK; });
+  EXPECT_CALL(mock, QueryInterface(_, _)).Times(1);
+  EXPECT_CALL(mock, AddRef()).Times(1);
+  EXPECT_CALL(mock, Release()).Times(2);
+
+  mock.AddRef();
+  pacemaker::inca::detail::unique_com_ptr<IDispatch> idispatch{&mock};
+  EXPECT_NO_THROW(ConcreteProxy proxy{std::move(idispatch)});
+}
+
+TEST_F(TC002, F)
+{
+  MockCOM mock;
+  mock.Delegate_QueryInterface([&mock](const IID &, void **out)
+                               {*out = reinterpret_cast<void*>(&mock); return S_OK; });
+  EXPECT_CALL(mock, AddRef()).Times(1);
+  EXPECT_CALL(mock, Release()).Times(2);
+
+  EXPECT_CALL(mock, QueryInterface(_, _)).Times(1);
+
+  mock.AddRef();
+  pacemaker::inca::detail::unique_com_ptr<IDispatch> idispatch{&mock};
+  ConcreteProxy                                      proxy{std::move(idispatch)};
+}
+
+TEST_F(TC002, G)
+{
+  MockCOM mock;
+  mock.Delegate_QueryInterface([&mock](const IID &, void **out)
+                               {*out = reinterpret_cast<void*>(&mock); return E_NOINTERFACE; });
+  EXPECT_CALL(mock, AddRef()).Times(1);
+  EXPECT_CALL(mock, Release()).Times(1);
+  EXPECT_CALL(mock, QueryInterface(_, _)).Times(1);
+
+  mock.AddRef();
+  pacemaker::inca::detail::unique_com_ptr<IDispatch> idispatch{&mock};
+  EXPECT_THROW(ConcreteProxy proxy{std::move(idispatch)}, std::runtime_error);
 }
